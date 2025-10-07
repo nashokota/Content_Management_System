22ec7893352344f41a98b6f241ca8f2c7b5acdcc
nashokota <preom.cc.bd@gmail.com>
2025-10-07 20:24:28 +0600

    Connecting Form UI to server (completed)

--- website - frontend/src/pages/userAuthForm.page.jsx
+++ website - frontend/src/pages/userAuthForm.page.jsx
@@ -5,16 +5,18 @@ import { Link } from "react-router-dom";
 import AnimationWrapper from "../common/page-animation";
 import { Toaster, toast } from "react-hot-toast";
 import axios from "axios";
+import { storeInSession } from "../common/session";
 
 const UserAuthForm = ({ type }) => {
-  const authForm = useRef();
 
   let serverRoute = type === "sign-in" ? "/signin" : "/signup";
 
   const userAuthThroughServer = (serverRoute, formData) => {
 
     axios.post(import.meta.env.VITE_SERVER_DOMAIN + serverRoute, formData).then(({data}) => {
-      
+      storeInSession("user", JSON.stringify(data));
+    }).catch(({ response }) =>{
+      toast.error(response.data.error);
     })
   };
 
@@ -23,8 +25,9 @@ const UserAuthForm = ({ type }) => {
 
     let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
     let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
+
     //formData
-    let form = new FormData(authForm.current);
+    let form = new FormData(formElement);
     let formData = {};
 
     for (let [key, value] of form.entries()) {
@@ -58,7 +61,7 @@ const UserAuthForm = ({ type }) => {
     <AnimationWrapper keyvalue={type}>
       <section className="h-cover flex items-center justify-center">
         <Toaster />
-        <form ref={authForm} className="w-[80%] max-w-[400px]">
+        <form id="formElement" className="w-[80%] max-w-[400px]">
           <h1 className="text-4xl font-gelasio capitalize text-center md-24">
             {type === "sign-in" ? "Welcome back" : "Join us today!"}
           </h1>
