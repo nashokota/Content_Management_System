3f95982fe54deccfeeb10bc7a7e6324db41781e8
nashokota <preom.cc.bd@gmail.com>
2025-10-09 02:12:41 +0600

    Blog Editor -Navbar

--- website - frontend/src/components/blog-editor.component.jsx
+++ website - frontend/src/components/blog-editor.component.jsx
@@ -1,6 +1,27 @@
+import { Link } from "react-router-dom";
+import logo from "../imgs/logo.png";
 const BlogEditor = () => {
     return (
-        <div>BlogEditor From Component JSX file</div>
+        <>
+        <nav  className="navbar">
+            <Link to="/" className="flex-none w-10">
+                <img src={logo} />
+            </Link>
+            <p className="max-md:hidden text-black line-clamp-1 w-full">
+                New Blog
+            </p>
+
+            <div className="flex gap-4 ml-auto">
+                <button className="btn-dark py-2">
+                    Publish
+                </button>
+                <button className="btn-light py-2">
+                    Save
+                </button>
+            </div>
+        </nav>
+        </>
+        
     )
 }
 
