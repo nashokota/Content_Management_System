07f22ff763e186db1f0779a694d1668d5cb5497d
nashokota <preom.cc.bd@gmail.com>
2025-10-10 22:00:54 +0600

    frontend of publish form page

--- website - frontend/src/components/blog-editor.component.jsx
+++ website - frontend/src/components/blog-editor.component.jsx
@@ -19,7 +19,7 @@ const BlogEditor = () => {
   useEffect(() => {
       setTextEditor(new EditorJS({
           holderId: "textEditor",
-          data:'',
+          data: content,
           tools: tools,
           placeholder: "Write your blog here...",
       }))
@@ -152,7 +152,7 @@ const BlogEditor = () => {
               className="text-4xl font-medium w-full h-20 outline-none resize-none mt-10 leading-tight placeholder:opacity-40"
               onKeyDown={handleTitleKeyDown}
               onChange={handleTitleChange}
-              value={title || ""}
+              defaultValue={title}
             ></textarea>
 
             <hr className="w-full opacity-10 my-5"/>
