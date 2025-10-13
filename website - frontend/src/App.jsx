e14ce1e6f392871534ba7ee9e0fe95e4dcda8b70
nashokota <preom.cc.bd@gmail.com>
2025-10-13 18:45:30 +0600

    setting up homepage route + homepage - InPageNavigation

--- website - frontend/src/App.jsx
+++ website - frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Editor from "./pages/editor.pages";
 import UserAuthForm from "./pages/userAuthForm.page";
 import { createContext, useState, useEffect } from "react";
 import { lookInSession } from "./common/session";
+import HomePage from "./pages/home.page";
 
 export const UserContext = createContext({})
 
@@ -22,6 +23,7 @@ const App = () => {
             <Routes>
                 <Route path="/editor" element={ <Editor /> } />
                 <Route path="/" element={<Navbar />} >
+                    <Route index element={<HomePage/>}/>
                     <Route path="signin" element={<UserAuthForm type="sign-in" />} />
                     <Route path="signup" element={<UserAuthForm type="sign-up" />} />
                 </Route>
