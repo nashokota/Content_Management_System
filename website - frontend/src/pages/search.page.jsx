2efed60e887356c06ec5fbed7b4230782a305039
nashokota <preom.cc.bd@gmail.com>
2025-10-16 03:03:35 +0600

    Search User card function

--- website - frontend/src/pages/search.page.jsx
+++ website - frontend/src/pages/search.page.jsx
@@ -8,12 +8,14 @@ import BlogPostCard from "../components/blog-post.component";
 import LoadMoreDataBtn from "../components/load-more.component";
 import axios from "axios";
 import { filterPaginationData } from "../common/filter-pagination-data";
+import UserCard from "../components/usercard.component";
 
 const SearchPage = () => {
 
     let {query} = useParams();
 
     let [blogs, setBlog] = useState(null);
+    let [users, setUsers] = useState(null);
 
     const searchBlogs = ({page = 1, create_new_arr = false}) => {
         
@@ -34,14 +36,43 @@ const SearchPage = () => {
         });
     }
 
+    const fetchUsers = () => {
+        axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/search-users", { query })
+        .then(({data: { users } } )  => {
+            setUsers(users);
+        }).catch(err => {
+            console.log(err);
+        });
+    }
+
     useEffect(() => {
         resetState();
         searchBlogs({page: 1, create_new_arr: true});
+        fetchUsers();
     }, [query]);
 
     const resetState = () => {
         setBlog(null);
+        setUsers(null);
     }
+
+    const UserCardWrapper =() =>{
+        return(
+            <>
+                {
+                    users == null ? <Loader/>
+                    :users.length ? 
+                        users.map((user,i) => {
+                            return <AnimationWrapper key={i} transition={{duration: 1, delay: i*0.08}}>
+                                <UserCard user={user}/>
+                            </AnimationWrapper>
+                           })                       
+                    : <NoDataMessage message={"No user found"}/>
+                }
+            </>
+        )
+    }
+
     return (
         <section className="h-cover flex justify-center gap-10">
             <div className="w-full">
@@ -59,8 +90,15 @@ const SearchPage = () => {
                         }
                         {<LoadMoreDataBtn state={blogs} fetchDataFun={searchBlogs}/>}
                     </>
+                    <UserCardWrapper/> 
                 </InPageNavigation>
             </div>
+
+            <div className="min-w-[40%] lg:main-w-[350px] max-w-min border-l border-grey pl-8 pt-3 max-md:hidden">
+                    <h1 className="font-medium text-xl mb-8">User related to search <i className="fi fi-rr-circle-user mt-1"></i></h1>
+                    <UserCardWrapper/>
+            </div>
+
         </section>
     )
 }
