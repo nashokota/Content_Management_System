6edbbbe5deeacefeed0314e64a3cf737a195ed34
nashokota <preom.cc.bd@gmail.com>
2025-10-16 15:30:17 +0600

    Fetching blog details data + Blog page frontend

---
+++ website - frontend/src/components/blog-interaction.component.jsx
@@ -0,0 +1,9 @@
+const BlogInteraction = () => {
+    return (
+        <div className="blog-interaction-container">
+            <h1>This is a blog interaction component</h1>
+        </div>
+    )
+}
+
+export default BlogInteraction;
--- website - frontend/src/pages/blog.page.jsx
+++ website - frontend/src/pages/blog.page.jsx
@@ -1,10 +1,78 @@
-import { useParams } from "react-router-dom";
+import axios from "axios";
+import { Activity } from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import AnimationWrapper from "../common/page-animation";
+import Loader from "../components/loader.component";
+import { getDay } from "../common/date";
+import BlogInteraction from "../components/blog-interaction.component";
+
+export const blogDataStructure = {
+    title: '',
+    des: '',
+    content: [],
+    tags: [],
+    author: { personal_info:{}},
+    banner: '',
+    publishedAt: '',
+}
 
 const BlogPage = () => {
 
     let {blog_id} =useParams();
+
+    const [blog, setBlog] = useState(blogDataStructure);
+    const [ loading, setLoading ] = useState(true);
+
+    let {title, content, banner, author:{personal_info:{fullname, username:author_username, profile_img}}, publishedAt} = blog;
+
+    const fetchBlog = () => {
+        axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/get-blog", { blog_id })
+        .then(({data: {blog}}) => {
+            setBlog(blog);
+            setLoading(false);
+        })
+        .catch(err => {
+            console.log(err);
+            setLoading(false);
+        });
+    }
+
+    useEffect(() => {
+        fetchBlog();
+    }, []);
+
     return (
-        <h1>This is a blog page for - {blog_id}</h1>
+        <AnimationWrapper>
+            {
+                loading ? <Loader/>
+                :
+                <div className="max-w-[900px] center py-10 max-lg:px-[5vw]">
+                    <img src={banner} className="aspect-video"/>
+                    <div className="mt-12">
+                        <h2>{title}</h2>
+                        <div className="flex max-sm:flex-col justify-between my-8">
+                            <div className="flex gap-5 items-start">
+                                <img src={profile_img} className="w-12 h-12 rounded-full"/>
+                                <p className="capitalize">
+                                    {fullname}
+                                    <br />
+                                    @
+                                    <Link to={`/user/${author_username}`} 
+                                    className="underline">{author_username}
+                                    </Link>
+                                </p>
+                            </div>
+                            <p className="text-dark-grey opacity-75 max-sm:mt-6 max-sm:ml-12 max-sm:pl-5">Published on {getDay(publishedAt)}</p>
+                        </div>
+                    </div>
+
+                    <BlogInteraction />
+
+                </div>
+            }
+        </AnimationWrapper>
     )
 }
 
